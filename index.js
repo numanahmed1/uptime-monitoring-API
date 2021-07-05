1e39be96c0762d621fd3c5eb28f7da1a223c1f5a
numanahmed1 <numanahmed317@gmail.com>
2021-07-05 23:07:19 +0600

    uptime-monitoring-API completed

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 /*
- * Title: Uptime monitoring API
- * Description: A RESTful API to monitor up or down time of user defined links
+ * Title: Initial file
+ * Description: Initial file to start the server and workers
  * Author: Numan Ahmed
- * Date: 29-06-2021
+ * Date: 04-07-2021
  */
 
 // dependencies
-const http = require("http");
-const { handleReqRes } = require("./helpers/handleReqRes");
-const environment = require("./helpers/environment");
-const data = require("./lib/data");
-const { sendTwilioSMS } = require("./helpers/notification");
+const server = require("./lib/server");
+const worker = require("./lib/worker");
 
 // app object module scaffolding
 const app = {};
 
-// checking sms
-sendTwilioSMS("01790314627", "First time sending sms using NODE.js", (err) => {
-  console.log("Error from Twilio", err);
-});
-
-// create server
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(environment.port, () => {
-    console.log(`Listening to port number ${environment.port}`);
-  });
+app.init = () => {
+  // start the server
+  server.init();
+  // start the worker
+  worker.init();
 };
 
-// handle request and response
-app.handleReqRes = handleReqRes;
+app.init();
 
-// Start the server
-app.createServer();
+module.exports = app;
